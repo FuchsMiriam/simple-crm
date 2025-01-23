68a7b139ec6526bd547a45a13ef7d4aa8f429765
Miriam Fuchs <fuchsmiriam@yahoo.com>
2025-01-23 17:58:38 +0100

    Add environment.ts to .gitignore

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    apiKey: "AIzaSyCqMNFL6EOBzB-WEiP4oWYmWn11kxFMK90",
-    authDomain: "simple-crm-4180e.firebaseapp.com",
-    projectId: "simple-crm-4180e",
-    storageBucket: "simple-crm-4180e.firebasestorage.app",
-    messagingSenderId: "253740735379",
-    appId: "1:253740735379:web:9154bde3e4c292679de666"
-  };
